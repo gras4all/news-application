e5136ff8ccb42217312a718c763d6550c1758085
Andrey Grunenkov <60237240+gras4all@users.noreply.github.com>
2022-08-15 14:00:06 +0400

    Initial Commit

---
+++ mvpExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvpExample
+//
+//  Created by Андрей Груненков on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
